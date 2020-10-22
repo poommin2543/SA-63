25fea94fbd2ecbb976aa5b20c44435a71edd4cc6
Poommin Phinphimai <b6111090@office365.sut.ac.th>
2020-10-22 18:29:08 +0700

    Ui update Table

--- frontend/plugins/welcome/src/components/afterlogin/afterlogin.tsx
+++ frontend/plugins/welcome/src/components/afterlogin/afterlogin.tsx
@@ -2,7 +2,23 @@ import React, { FC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import ComponanceTable from '../Table';
 import Button from '@material-ui/core/Button';
- 
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TableCell from '@material-ui/core/TableCell';
+import FormControl from '@material-ui/core/FormControl';
+
+
 import {
  Content,
  Header,
@@ -11,29 +27,103 @@ import {
  ContentHeader,
  Link,
 } from '@backstage/core';
+import { Hidden } from '@material-ui/core';
  
-const afterlogin: FC<{}> = () => {
- const profile = { givenName: 'to ระบบเครื่องมือแพทย์' };
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar color="secondary" position="sticky" elevation={0}>
+        <Toolbar>
+          <Grid container spacing={1} alignItems="center">
+            <Hidden smUp>
+            <Grid item>
+                
+              </Grid>
+            </Hidden>
+            <Grid item xs />
+            <Grid item>
+            
+            </Grid>
+            
+            <Grid item>
+            <Button  variant="outlined" color="inherit" 
+            size="small" component={RouterLink}
+            to="/user">
+                เพิ่มข้อมูล
+              </Button>
+                </Grid>
+                <Grid item>
+            <Button variant="outlined" color="inherit" 
+            size="small" component={RouterLink}
+            to="/">
+                logout
+              </Button>
+                </Grid>  
+            <Grid item>
+            
+            </Grid>
+            
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <AppBar
+        component="div"
+        color="secondary"
+        position="static"
+        elevation={0}
+      >
+        <Toolbar>
+          <Grid container alignItems="center" spacing={1}>
+            <Grid item xs>
+              <Typography color="inherit" variant="h2" component="h2">
+                รายการบันทึกข้อมูล
+              </Typography>
+            </Grid>
+            <Grid item>
+            
+                </Grid>  
+            
+            <Grid item>
+            
+            </Grid>
+            
+          </Grid>
+        </Toolbar>
+      </AppBar>
+        <ComponanceTable></ComponanceTable>
+      
+    </div>
+  );
+}
  
- return (
-   <Page theme={pageTheme.home}>
-     <Header
-       title={`Welcome ${profile.givenName || 'to Backstage'}`}
-       subtitle="ระบบหลัก : ระบบข้อมูลแพทย์"
-     ></Header>
-     <Content>
-       <ContentHeader title="บันทึกข้อมูล">
-         <Link component={RouterLink} to="/user">
-           <Button variant="contained" color="primary">
-             Add User
-           </Button>
-         </Link>
-       </ContentHeader>
-       <ComponanceTable></ComponanceTable>
-     </Content>
-     
-   </Page>
- );
-};
- 
-export default afterlogin;
